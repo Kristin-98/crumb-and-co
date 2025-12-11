f83946bbd7bdf8c0141b44252afdc833d3b73c02
Kristin-98 <kristi98_s@hotmail.se>
2025-12-11 17:30:49 +0100

    changed hero image + animation on text

--- components/ui/layout/hero.tsx
+++ components/ui/layout/hero.tsx
@@ -1,24 +1,28 @@
+"use client";
+import { motion } from "framer-motion";
 import { ChevronRight } from "lucide-react";
 import Image from "next/image";
-import { Button } from "../button";
 
 export default function Hero() {
   return (
     <>
       <div className="relative md:w-[calc(100vw-4rem)] w-[calc(100vw-2rem)] md:h-[calc(95vh-4rem)] h-[calc(100vh-2rem)] rounded-3xl overflow-hidden">
-        <Image src="/hero.png" alt="cafe" fill className="object-cover" />
-        <div className="absolute inset-0 flex justify-center items-center">
-          <div className=" flex-col flex justify-center items-center rounded-3xl bg-background m-2 p-2 md:p-4 md:w-130 h-50 text-center">
-            <h1 className="md:text-2xl text-lg md:font-thin pb-4">
-              Subscribe today and treat yourself to handcrafted bakery boxes
-              every month.
-            </h1>
-            <Button className="group bg-primary text-white font-light rounded-full py-2 md:px-6 px-4 hover:bg-accent-foreground">
-              Join our monthly subscription.
-              <ChevronRight className="transition-transform duration-300 group-hover:translate-x-2" />
-            </Button>
-          </div>
-        </div>
+        <Image src="/hero4.png" alt="cafe" fill className="object-cover" />
+        <motion.div
+          className="absolute right-64 top-2/5 flex justify-center flex-col items-center md:text-5xl font-extrabold text-accent"
+          initial={{ opacity: 0, y: 40 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1.5, ease: "easeOut" }}
+        >
+          <h1>Where</h1>
+          <p>every bite</p>
+          <p className="pb-4">counts</p>
+
+          <button className="group bg-primary text-lg text-white font-medium rounded-full py-2 md:px-4 px-3 hover:bg-accent-foreground flex flex-row items-center">
+            Join our monthly subscription.
+            <ChevronRight className="transition-transform duration-300 group-hover:translate-x-2" />
+          </button>
+        </motion.div>
       </div>
     </>
   );
