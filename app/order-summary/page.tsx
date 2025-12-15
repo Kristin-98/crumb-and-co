aac98907a2b46d2ab354dd8a6b83f4c9dc25deba
Kristin-98 <kristi98_s@hotmail.se>
2025-12-15 15:56:24 +0100

    added a fieldset for adress

--- app/order-summary/page.tsx
+++ app/order-summary/page.tsx
@@ -1,9 +1,11 @@
 import Subscribe from "@/components/ui/layout/subscribe";
+import { AdressFieldset } from "@/components/ui/layout/subscribe-adress-field";
 
 export default function OrderSummary() {
   return (
     <>
       <Subscribe />
+      <AdressFieldset />
     </>
   );
 }
